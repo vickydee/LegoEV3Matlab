ae0a6b4d671e15b64b8c85b4855cfe2ee86f86bb
vickydee <79496219+vickydee@users.noreply.github.com>
2023-10-18 14:33:01 -0700

    Add files via upload

---
+++ connectColorSensor.m
@@ -0,0 +1,23 @@
+%% Write 3 if statements. One for color. 
+%%mycolorsensor = colorSensor('HIM',inputport)
+brick.SetColorMode(2,2);
+color = brick.ColorCode(2);
+%%0	No color (Unknown color) 
+%%1	Black 
+%%2	Blue 
+%%3	Green 
+%%4	Yellow 
+%%5	Red 
+%%6	White 
+%%7	Brown
+while (color ~= 0)
+    
+         sprintf("Color: %d", color);  
+    
+end
+%% Move Motor
+brick.MoveMotor('BD', 50);
+if(color == 5)
+   pause(1);
+end
+distance = brick.UltrasonicDist(SensorPort);
