776a00644aab4166ea0573e426d3c5c33a1f57a8
GerudoShield <143758520+GerudoShield@users.noreply.github.com>
2023-11-06 08:02:07 -0700

    Update colorSensor.m

--- colorSensor.m
+++ colorSensor.m
@@ -13,8 +13,23 @@ while (color ~= 0)
         run('stop.m');
         pause(1)
     end
+
+      %% for green
+    if(color== 3)
+        brick.beep();
+        run('stop.m');
+        pause(1);
+        run('moveForward.m');
+    end
+%% for yellow
     if (color == 4)
         %%trigger remote control
+        brick.beep();
+        run('stop.m');
+        pause(1);
+        run('moveForward.m');
+    end
+
     end
     if (color == 2)
         pause(1)
