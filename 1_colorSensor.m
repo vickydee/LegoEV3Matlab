653c1aa9bb719fbdbbe0e0638427b4d76c57180a
GerudoShield <143758520+GerudoShield@users.noreply.github.com>
2023-11-15 14:30:26 -0700

    Update 1_colorSensor.m

--- 1_colorSensor.m
+++ 1_colorSensor.m
@@ -3,22 +3,22 @@ color = brick.ColorCode(1);
  %% red
      if(color == 5)
        brick.beep;
-       run('stop.m');
+       run('1_stop.m');
        pause(3);
      end
 
 %% blue
     if(color == 2)
         disp("blue");
-        run('stop.m');
-        run('remotecontrol.m');
+        run('1_stop.m');
+        run('2_remotecontrol.m');
     end
 %% green
    if (color == 3)
         brick.beep;
         brick.beep;
         brick.beep;
-        run('stop.m');
+        run('1_stop.m');
         pause(6);
     end
 %% yellow
